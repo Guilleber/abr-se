680bd546d8039bb151d0d2ff65d755413c04abba
Guillaume Le Berre <leberre_guillaume@orange.fr>
2016-11-09 13:24:41 +0100

    Incorporated Tests

---
+++ abr-se/src/abr_se/ABR.java
@@ -0,0 +1,17 @@
+package abr_se;
+
+public class ABR {
+	public class Node {
+		private int value;
+		public Node childLeft = null;
+		public Node childRight = null;
+		
+		Node(int value) {
+			this.value = value;
+		}
+	}
+	
+	private Node root = null;
+	
+	ABR() {}
+}
