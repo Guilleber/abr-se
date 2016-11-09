5120c49614f792031e28108da0c71835b39ac4c7
Guillaume Le Berre <leberre_guillaume@orange.fr>
2016-11-09 13:37:12 +0100

    insert + isEmpty

--- abr-se/src/abr_se/ABR.java
+++ abr-se/src/abr_se/ABR.java
@@ -9,9 +9,47 @@ public class ABR {
 		Node(int value) {
 			this.value = value;
 		}
+		
+		public int getValue() {
+			return value;
+		}
 	}
 	
 	private Node root = null;
+	private int nbElements = 0;
 	
 	ABR() {}
+	
+	public void insert(int value) {
+		if(root == null) {
+			root = new Node(value);
+			nbElements += 1;
+		}
+		else
+			insert(value, root);
+	}
+	
+	private void insert(int value, Node node) {
+		if(value != node.getValue()) {
+			if(value > node.getValue()) {
+				if(node.childRight == null) {
+					node.childRight = new Node(value);
+					nbElements += 1;
+				}
+				else
+					insert(value, node.childRight);
+			} else {
+				if(node.childLeft == null) {
+					node.childLeft = new Node(value);
+					nbElements += 1;
+				}
+				else
+					insert(value, node.childLeft);
+			}
+		}
+	}
+	
+	public boolean isEmpty() {
+		return root == null;
+	}
 }
